6c08d87c91ad27e3fb70e2aad5efcef6287c2f36
RosieMable <sciarrillo.nella@gmail.com>
2019-09-12 20:22:32 +0200

    Project Set Up

---
+++ TacticsRPG/Assets/Scripts/Components/Tile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    //Four steps are one tile
+    public const float stepHeight = .25f;
+
+    //Track its own position and height
+    public Point pos;
+    public int height;
+
+    //Convenience property called Center
+    //Lets place other objects in the center of the top of the tile
+    public Vector3 center { get { return new Vector3(pos.x, height * stepHeight, pos.y); } }
+
+    //When the height is modified, then the tiles are visually updated
+    void Match()
+    {
+        transform.localPosition = new Vector3(pos.x, height * stepHeight / 2f, pos.y);
+        transform.localScale = new Vector3(1, height * stepHeight, 1);
+    }
+
+    //The board creator will create boards by randomly growing and/or shrinking tiles
+    //Methods to shrink and grow
+
+    public void Grow()
+    {
+        height++;
+        Match();
+    }
+
+    public void Shrink()
+    {
+        height--;
+        Match();
+    }
+
+    //Method called Load, overloaded to accept various parameter types
+    //Makes it easier to persist the Tile data as a Vector3
+
+    public void Load (Point p, int h)
+    {
+        pos = p;
+        height = h;
+        Match();
+    }
+
+    public void Load (Vector3 v)
+    {
+        Load(new Point((int)v.x, (int)v.z), (int)v.y);
+    }
+}
---
+++ TacticsRPG/Assets/Scripts/DataContainer/Point.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Point
+{
+    public int x;
+    public int y;
+
+    //Constructor
+    public Point (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    //Operators Overloading
+    //Plus +
+    public static Point operator + (Point a, Point b)
+    {
+        return new Point(a.x + b.x, a.y + b.y);
+    }
+    //Minus -
+    public static Point operator - (Point p1, Point p2)
+    {
+        return new Point(p1.x - p2.x, p1.y - p2.y);
+    }
+    //Equals ==
+    public static bool operator == (Point a, Point b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+
+    //Not equal !=
+    public static bool operator != (Point a, Point b)
+    {
+        return !(a == b);
+    }
+
+    //we have implemented the equality operator, 
+    //it is expected that we will also override Equals and GetHashCode
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Point)
+        {
+            Point p = (Point)obj;
+            return x == p.x && y == p.y;
+        }
+        return false;
+    }
+
+    public bool Equals (Point p)
+    {
+        return x == p.x && y == p.y;
+    }
+
+    public override int GetHashCode()
+    {
+        return x ^ y;
+    }
+
+    /*
+     * Rather than having to manually print out the x and y values
+     * of our struct, it would be nice to be able to just log
+     * the Point and let it determine 
+     * how to print out its own contents.
+     * */
+
+    public override string ToString()
+    {
+        return string.Format("({0},{1})", x, y);
+    }
+}
