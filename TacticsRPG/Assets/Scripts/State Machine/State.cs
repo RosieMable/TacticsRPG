973c9c8d8bbf23b6731c9cc64a8acc55e7d560e4
RosieMable <sciarrillo.nella@gmail.com>
2019-09-14 00:02:27 +0200

    State Machine + Camera Rig

---
+++ TacticsRPG/Assets/Scripts/State Machine/State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//Used as a template class
+//The other states should be concrete and inherite from this
+public abstract class State : MonoBehaviour
+{
+    //The idea is that if the state changes, it will call Enter and Exit methods
+    public virtual void Enter()
+    {
+        AddListeners();
+    }
+
+    public virtual void Exit()
+    {
+        RemoveListeners();
+    }
+
+    protected virtual void OnDestroy()
+    {
+        RemoveListeners();
+    }
+
+    protected virtual void AddListeners() {
+
+    }
+
+    protected virtual void RemoveListeners()
+    {
+
+    }
+}
